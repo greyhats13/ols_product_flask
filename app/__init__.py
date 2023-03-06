2cfa37906779c9a3975c23a8a6722500ee2a2436
greyhats <arief.rahman@cloudcomrade.com>
2023-03-06 20:52:32 +0700

    add __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from app.config import Config
+from app.helpers.databases.mysql import MySQLHelper
+from flask_migrate import Migrate
+# from app.helpers.databases.redis import RedisHelper
+from flask_restful import Api
+from flask_healthz import healthz
+
+app = Flask(__name__)
+app.config.from_object(Config)
+mysql = MySQLHelper(app)
+mysql_migrate = Migrate(app, mysql.db)
+# redis = RedisHelper(app)
+api = Api(app)
+
+from app import routes
